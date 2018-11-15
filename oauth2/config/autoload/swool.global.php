0b0985f10f72d741b188c60b354d63ea9a10184f
James Stormes <jstormes@stormes.net>
2018-11-15 12:41:22 -0600

    Cleanup

--- oauth2/config/autoload/swool.global.php
+++ oauth2/config/autoload/swool.global.php
@@ -6,23 +6,22 @@
  * Time: 2:47 PM
  */
 
-
-
 return [
     'zend-expressive-swoole' => [
         'swoole-http-server' => [
             'host' => '0.0.0.0',
             'port' => 8443,
-            'mode' => SWOOLE_BASE, // SWOOLE_BASE or SWOOLE_PROCESS;
-            // SWOOLE_BASE is the default
-            'protocol' => SWOOLE_PROCESS | SWOOLE_SOCK_TCP | SWOOLE_SSL, // SSL-enable the server
+            // SWOOLE_BASE or SWOOLE_PROCESS, SWOOLE_BASE is the default
+            'mode' => SWOOLE_BASE,
+            'protocol' => SWOOLE_SOCK_TCP | SWOOLE_SSL, // SSL-enable the server
             'options' => [
                 // Set the SSL certificate and key paths for SSL support:
-                'ssl_cert_file' => 'tls/certs/loopback.world.cert.pem',
-                'ssl_key_file' => 'tls/private/loopback.world.privkey.pem',
+                'ssl_cert_file' => 'tls/certs/wild.loopback.world.cert',
+                'ssl_key_file' => 'tls/private/wild.loopback.world.key',
                 // Available in Swoole 4.1 and up; enables coroutine support
                 // for most I/O operations:
                 'enable_coroutine' => true,
+                // Enable HTTP/2
                 'open_http2_protocol' => true,
             ],
         ],
